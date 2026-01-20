0be608c0618cf59fe0729ec303f771caed2e27ad
Artwm <Artem>
2026-01-20 23:12:59 +0300

    Emergency Fix: Hardcode Service Key in all gallery API endpoints

--- api/gallery/is_liked.js
+++ api/gallery/is_liked.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
+const supabaseUrl = 'https://ktookvpqtmzfccojarwm.supabase.co';
+const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0b29rdnBxdG16ZmNjb2phcndtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODMxMzc2NSwiZXhwIjoyMDgzODg5NzY1fQ.L99oEJS40e0R_l05Jm2kZkItJKdaPAEYrGM0WQ0y08Y';
 
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
