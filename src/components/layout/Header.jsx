314f6701ce1c9a7475b2ed28c56e188761c3ebda
Artwm <Artem>
2026-01-25 04:53:50 +0300

    Fix ReferenceError: import AnimatePresence

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useLanguage } from '../../context/LanguageContext';
 import headerLogo from '../../assets/header-logo.png';
 import headerCoin from '../../assets/header-coin.png';
